fcf50a79040511214b7319b52861cd606dbef8de
Andreas Herman <andreasherman@Andreass-MacBook-Pro.local>
2019-05-12 14:42:46 +0200

    import and req fix

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,2 +1,6 @@
 from .document import Document
-from .formating import TextFormater, DocumentFormater, TagTextFormater, HTMLDocumentFormater, WebPageHTMLDocumentFormater
+from .formating import TextFormater
+from .formating import DocumentFormater
+from .formating import TagTextFormater
+from .formating import HTMLDocumentFormater
+from .formating import WebPageHTMLDocumentFormater
